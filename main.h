a6edf1cb97b1e080299597377e6d43eae3a57b92
zahraiva <zaraiv06@gmail.com>
2024-07-26 14:00:08 +0400

    prototypes

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list ptr);
+int print_str(va_list ptr);
+int print_dec(va_list ptr);
+#endif
