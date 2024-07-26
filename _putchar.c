28fc6e9aa01b18a6655584d6b4ece6b033eaf5db
zahraiva <zaraiv06@gmail.com>
2024-07-26 14:04:13 +0400

    create putchar

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - Print the parameter
+ * @c: Character
+ * Return: Always 0
+ * -------------------------------------------------------------
+ * Source File: _putchar.c - Print the character
+ */
+int _putchar(char c)
+{
+  return (write(1, &c, 1));
+}
