f9aa2db842de33a028145f9b22df94b82cb25be5
Your Name <you@example.com>
2024-07-28 09:42:09 +0400

    fixed

--- print_dec.c
+++ print_dec.c
@@ -6,10 +6,10 @@
  */
 int print_dec(va_list ptr)
 {
-	char *b;
+	int b;
 	int i;
 
-	b = va_arg(ptr, char*);
+	b = va_arg(ptr, int);
 
 	if (b < 0)
 	{
@@ -22,5 +22,30 @@ int print_dec(va_list ptr)
 		print_dec(b / 10);
 
 	_putchar(b % 10 + '0');
+	return (i);
+
+}
+/**
+ * print_dec_int - function
+ * @b: variable
+ * Return: i
+*/
+int print_dec_int(int b)
+{
+	int count = 0;
+
+	if (b < 0)
+	{
+		_putchar('-');
+		i++;
+		b = -b;
+	}
+
+	if (b / 10)
+		i += print_dec_int(b / 10);
+
+	_putchar(b % 10 + '0');
+	i++;
+
 	return (i);
 }
