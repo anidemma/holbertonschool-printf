e09b6b89150a85f62679b7489b9ac9f4da56c0eb
Your Name <you@example.com>
2024-07-28 08:32:42 +0400

    fixed

--- print_dec.c
+++ print_dec.c
@@ -1,20 +1,26 @@
 #include "main.h"
+/**
+ * print_dec - function
+ * @ptr: va list
+ * Return: i
+ */
 int print_dec(va_list ptr)
 {
 	char *b;
 	int i;
 
 	b = va_arg(ptr, char*);
+
 	if (b < 0)
 	{
-		  _putchar('-');
-		    b = -1 * b;
+		_putchar('-');
+		b = -1 * b;
 	}
 
-	    i = b / 10;
-	        if (i)
-			      print_dec(b / 10);
+	i = b / 10;
+	if (i)
+		print_dec(b / 10);
 
-		    _putchar(b % 10 + '0');
-		        return (i);
+	_putchar(b % 10 + '0');
+	return (i);
 }
