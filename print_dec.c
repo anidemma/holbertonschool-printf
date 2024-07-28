c35549096cfc24e1f718529b0d4f6e6cdaf9d469
Your Name <you@example.com>
2024-07-28 08:12:43 +0400

    decimal

---
+++ print_dec.c
@@ -0,0 +1,20 @@
+#include "main.h"
+int print_dec(va_list ptr)
+{
+	char *b;
+	int i;
+
+	b = va_arg(ptr, char*);
+	if (b < 0)
+	{
+		  _putchar('-');
+		    b = -1 * b;
+	}
+
+	    i = b / 10;
+	        if (i)
+			      print_dec(b / 10);
+
+		    _putchar(b % 10 + '0');
+		        return (i);
+}
