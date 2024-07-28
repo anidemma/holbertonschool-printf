a151cb51bccdc617f03f522f615cfaca007ef4e5
Your Name <you@example.com>
2024-07-28 10:32:57 +0400

    fixed

---
+++ print_dec.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_dec - function
+ * @ptr: va_list
+ * Return:i
+ */
+int print_dec(va_list ptr)
+{
+	char *b;
+	int i;
+
+	b = va_arg(ptr, char*);
+	if (b < 0)
+	{
+		_putchar('-');
+		b = -1 * b;
+	}
+
+	i = b / 10;
+	if (i)
+		print_dec(b / 10);
+
+	_putchar(b % 10 + '0');
+	return (i);
+}
